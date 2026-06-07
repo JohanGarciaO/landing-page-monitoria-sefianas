2cea054bddc9e6094100e4ec2b54b792905ba5df
Johan Garcia <52720415+JohanGarciaO@users.noreply.github.com>
2026-06-07 07:06:29 +0000

    chore: add `plans.js` module with plans of `Engrenando`

---
+++ plans.js
@@ -0,0 +1,55 @@
+const plans = {
+  megaCombo: {
+    id: "megaCombo",
+    name: "Mega Combo",
+    description:
+      "A preparação mais completa para concursos de enfermagem, incluindo EAGS-SEF e CAP-ENF.",
+    price: "390",
+    billing: "monthly",
+    badge: "Mais Completo",
+    features: [
+      "Enfermagem",
+      "Português",
+      "Redação",
+      "Mentoria",
+      "3 correções de redação por mês",
+    ],
+    url: "https://pay.plataformatutory.com.br/checkout/a71d6ea3-265e-4691-b63a-73746cbd7d5f",
+  },
+
+  eagsMentoria: {
+    id: "eagsMentoria",
+    name: "EAGS-SEF Mentoria",
+    description: "A preparação perfeita para quem foca no EAGS-SEF.",
+    price: "270",
+    billing: "monthly",
+    badge: "Recomendado",
+    features: ["Enfermagem", "Português", "Mentoria"],
+    url: "https://pay.plataformatutory.com.br/checkout/092d5c59-a2df-4cdf-baea-1ef99acdb8a9",
+  },
+
+  eags: {
+    id: "eags",
+    name: "EAGS-SEF",
+    description:
+      "A preparação melhor custo-benefício para quem foca no EAGS-SEF.",
+    price: "230",
+    billing: "monthly",
+    badge: "Recomendado",
+    features: ["Enfermagem", "Português"],
+    url: "https://pay.plataformatutory.com.br/checkout/8d40f3d2-e8a5-420f-bd4a-98a86312c1f5",
+  },
+
+  cap: {
+    id: "cap",
+    name: "CAP-ENF",
+    description: "A preparação perfeita para quem foca no CAP-ENF.",
+    price: "230",
+    billing: "monthly",
+    badge: "Recomendado",
+    features: ["Enfermagem", "Redação", "3 correções de redação por mês"],
+    url: "https://pay.plataformatutory.com.br/checkout/08395dc1-8b37-4c49-830e-7f2b7082d784",
+  },
+};
+
+export default plans;
